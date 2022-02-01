7c3f454e6b538dba3ef86d9c68a69f6adf6d0582
Roman Kuzmenko <83546337+KuzmenkoRomanJS@users.noreply.github.com>
2022-02-01 20:09:31 +0200

    Add files, 1st day

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+import start from "./modules/start.js";
+import getFormPerson from "./modules/formPerson.js";
+
+const init = (selectorApp, title) => {
+  const app = document.querySelector(selectorApp);
+
+  const { main, firstForm } = start(app, title);
+
+  firstForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const forms = getFormPerson(firstForm.count.value);
+    firstForm.remove();
+
+    main.append(...forms);
+  });
+};
+
+init(".app", "Выбирите тур");
